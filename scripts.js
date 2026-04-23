24ddb6ff09c282b097227e5c64949115fe6e570e
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-23 09:48:20 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("video-header").addEventListener("click", function() {
+    alert("Memutar: Visions of the Cathedral (Cinematic Gothic)");
+});
+
+document.getElementById("audio-header").addEventListener("click", function() {
+    alert("Mendengarkan: Hymns of Resonance (Symphonic Metal)");
+});
+
+document.getElementById("gambar-header").addEventListener("click", function() {
+    alert("Melihat: Detail Arsitektur Gothic");
+});
